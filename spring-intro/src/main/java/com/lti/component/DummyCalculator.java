db312863376c9d405f51657078a00e1e084e5feb
Saurav Sanyal <sanyal.s271@gmail.com>
2019-07-05 17:21:58 -0700

    commit

---
+++ spring-intro/src/main/java/com/lti/component/DummyCalculator.java
@@ -0,0 +1,12 @@
+package com.lti.component;
+
+public class DummyCalculator {
+	
+	public int doAdd(int x, int y) {
+		return x+y;
+	}
+	
+	public int doSubstract(int x, int y) {
+		return x-y;
+	}
+}
--- spring-intro/src/test/java/com/lti/test/HelloTest.java
+++ spring-intro/src/test/java/com/lti/test/HelloTest.java
@@ -3,6 +3,7 @@ package com.lti.test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.lti.component.DummyCalculator;
 import com.lti.component.HelloWorld;
 
 public class HelloTest {
@@ -12,6 +13,13 @@ public class HelloTest {
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
 		
 		HelloWorld hw = (HelloWorld) context.getBean("hellowworld");		//bean id
-		System.out.println(hw.sayHello("Majrul"));
+		System.out.println(hw.sayHello(" wolfie_boii"));
+		
+		DummyCalculator dc = (DummyCalculator) context.getBean("doAdd");
+		int x = dc.doAdd(2, 4);
+		int y = dc.doSubstract(2,6);
+		System.out.println(x);
+		System.out.println(y);
 	}
 }
+
