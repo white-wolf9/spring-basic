e2b743cbed6f13cc917345a97bf0acc3ba03fba2
Saurav Sanyal <sanyal.s271@gmail.com>
2019-07-08 10:33:15 -0700

    Commit

--- spring-db/src/main/java/com/lti/test/CarTest.java
+++ spring-db/src/main/java/com/lti/test/CarTest.java
@@ -1,4 +1,4 @@
-package com.lti.component;
+package com.lti.test;
 
 import static org.junit.Assert.*;
 
@@ -6,18 +6,21 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.lti.component.CarPart;
+import com.lti.component.CarPartsInventory;
+
 public class CarTest {
 
 	@Test
 	public void add() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
-		CarPartsInventory cp=(CarPartsInventory) context.getBean("carpartsImpl1");
+		CarPartsInventory cp=(CarPartsInventory) context.getBean("carPartsImpl2");
 		
 		CarPart p=new CarPart();
 		p.setPartNo(2);
-		p.setPartName("wheel");
-		p.setCarModel("Audi Q3");
-	    p.setQuantity(8);
+		p.setPartName("Side Mirror");
+		p.setCarModel("Maruti 800");
+	    p.setQuantity(2);
 	    cp.addNewPart(p);
 	}
 
